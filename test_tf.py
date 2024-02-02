f4913d99de87b502630e89713b368273fa8f6c42
Muff2n <richard.willis5@gmail.com>
2024-02-02 14:08:58 +0000

    add test scripts and pytorch install

---
+++ test_tf.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+# Check if TensorFlow is built with CUDA (GPU) support
+print("Is TensorFlow built with CUDA:", tf.test.is_built_with_cuda())
+
+# Check if GPUs are available
+gpus = tf.config.list_physical_devices('GPU')
+print("Number of GPUs available:", len(gpus))
+
+# Get the name and additional details of the first available GPU
+if gpus:
+    for gpu in gpus:
+       print("GPU details:", gpu)
+else:
+    print("No GPU detected")
+
