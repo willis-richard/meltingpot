32200b33d636417bcbd142254cca89d61514c0f5
Muff2n <richard.willis5@gmail.com>
2024-07-30 17:19:32 +0100

    After switch to recreating ppo each time and loading policy weights

--- reward_transfer/schelling.py
+++ reward_transfer/schelling.py
@@ -4,13 +4,15 @@ said to implement cooperation and defection
 """
 
 import argparse
+import logging
 import os
 
 import json
 import ray
 from ray.rllib.algorithms.ppo import PPO, PPOConfig
 from ray.rllib.utils.checkpoints import get_checkpoint_info
-from ray.rllib.policy.policy import Policy
+from ray.rllib.algorithms.callbacks import DefaultCallbacks
+from ray.rllib.policy.policy import Policy, PolicyID, PolicySpec
 from ray.tune.registry import register_env
 from ray.tune.analysis.experiment_analysis import ExperimentAnalysis
 
@@ -19,6 +21,7 @@ from meltingpot import substrate
 
 
 def main():
+  print("Start")
   parser = argparse.ArgumentParser(description=__doc__)
   parser.add_argument(
       "--experiment_state",
@@ -41,12 +44,16 @@ def main():
 
   args = parser.parse_args()
 
-  register_env("meltingpot", utils.env_creator)
+  print("Calling init")
+  ray.init(address="local",
+           num_cpus=4,
+           logging_level=logging.ERROR)
 
-  ray.init(address="local")
+  register_env("meltingpot", utils.env_creator)
 
   # fuck it: get the config from the env state and just update the number of players and self interest as needed
   # will need to load in the policies
+  print("Loading ExperimentAnalysis")
   experiment = ExperimentAnalysis(
       args.experiment_state,
       default_metric="env_runners/episode_reward_mean",
@@ -60,58 +67,73 @@ def main():
   config["env_config"]["roles"] = substrate_config.default_player_roles
 
   base_env = utils.env_creator(config["env_config"])
-
-
-  # load all the policies...
-
-  # load the algorithm state, so we have access to the config
-  # DO FROM_CHECKPOINT AND THEN ITERATIVELY ADD POLICY
-
+  aids = base_env._ordered_agent_ids
 
   config = config.resources(num_gpus=0)
   config = config.env_runners(
       num_env_runners=0,
       num_envs_per_env_runner=1,
+      # create_env_on_local_worker=True,
   )
   config = config.evaluation(
       evaluation_duration=1,
-      evaluation_num_env_runners=1,
+      evaluation_num_env_runners=2,
+      evaluation_interval=1,
   )
+  # # wtf wtf wtf
+  # config = config.framework("tf2")
 
+  # load all the policies...
+  # Start with all defect
+  print("Loading defect policies")
+  policies = dict((aid, Policy.from_checkpoint(os.path.join(args.defection_checkpoint, "policies", aid))) for aid in aids)
+
+  class LoadPolicyCallback(DefaultCallbacks):
+
+    def __init__(self):
+      super().__init__()
+
+    def on_create_policy(self, *, policy_id: PolicyID, policy: Policy) -> None:
+      """Callback run whenever a new policy is added to an algorithm.
+
+      Args:
+          policy_id: ID of the newly created policy.
+          policy: The policy just created.
+      """
+      policy.set_weights(policies[policy_id].get_weights())
+
+
+  config = config.callbacks(LoadPolicyCallback)
+  # config = config.multi_agent(policies=policies)
+
+  #            new="AlgorithmConfig.num_cpus_for_main_process",
+
+
+  print("Building PPO instance")
   ppo = config.build()
 
-  # Start with all defect
-  for role in base_env._ordered_agent_ids:
-    ppo.remove_policy(role)
-    policy = Policy.from_checkpoint(os.path.join(args.defection_checkpoint, "policies", role))
-    ppo.add_policy(policy_id=role, policy=policy)
-  # ppo.load_checkpoint(args.defection_checkpoint)
-
-  # update the config resources
-  # do I need to update env_runners?
-
-  # sweep over the possible policy pairings
-
-  # update the policy mapping functions and call evaluate
-
+  print("Running evaluate()")
   results = ppo.evaluate()
   print(results)
 
   i = 0
 
-  with open(f"n_c_{i}", mode="a", encoding="utf8") as f:
+  with open(f"n_c_{i}.json", mode="w", encoding="utf8") as f:
     json.dump(results, f)
 
-  for role in base_env._ordered_agent_ids:
-    ppo.remove_policy(role)
-    i += 1
-    policy = Policy.from_checkpoint(os.path.join(args.cooperation_checkpoint, "policies", role))
-    ppo.add_policy(policy_id=role, policy=policy)
+  # sweep over the possible policy pairings
+  for aid in aids:
+    print(f"Update cooperate policy {role}")
+    policies[aid] = Policy.from_checkpoint(os.path.join(args.cooperation_checkpoint, "policies", aid))
+    # config = config.multi_agent(policies=policies)
+    ppo = config.build()
 
+    print("Running evaluate()")
     results = ppo.evaluate()
     print(results)
 
-    with open(f"n_c_{i}", mode="a", encoding="utf8") as f:
+    i += 1
+    with open(f"n_c_{i}.json", mode="w", encoding="utf8") as f:
       json.dump(results, f)
 
 
