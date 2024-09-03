10b9a70ed9f94126d396d514e98c101ba377892a
Muff2n <richard.willis5@gmail.com>
2024-09-03 16:28:23 +0100

    last run_meltingpot.sh

--- run_meltingpot.sh
+++ run_meltingpot.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -l
 #SBATCH --output=/scratch/users/%u/%j.out
 #SBATCH --job-name=meltingpot
-#SBATCH --time=1-23
+#SBATCH --time=2-00
 #SBATCH --mem=120G
 #SBATCH --gpus 1
 #SBATCH --cpus-per-gpu 16
 #SBATCH --nodes=1
-#SBATCH --nodelist=erc-hpc-comp031,erc-hpc-comp035,erc-hpc-comp039
+# works
+#SBATCH --nodelist=erc-hpc-comp031,erc-hpc-comp033,erc-hpc-comp034,erc-hpc-comp038,erc-hpc-comp039
+# testing
+# #SBATCH --nodelist=erc-hpc-comp033,erc-hpc-comp037
 
 # #SBATCH --tasks-per-node=1
 # #SBATCH --ntasks 1
@@ -23,10 +26,9 @@ echo "Running on node: $(hostname)"
 source ~/mambaforge/etc/profile.d/conda.sh
 conda activate paper_2
 
-# module load cuda/11.8.0-gcc-11.4.0
+# module load cuda/11.8.0-gcc-13.2.0
 module load cuda/12.2.1-gcc-13.2.0
-# module load cudnn/8.2.4.15-11.4-gcc-13.2.0
-# module load cudnn/8.7.0.84-11.8-gcc-11.4.0
+# module load cudnn/8.7.0.84-11.8-gcc-13.2.0
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/lib64:$CUDNN_HOME/lib64
 
@@ -39,36 +41,26 @@ cd $PYTHONPATH
 # 30ca9764
 # shrooms
 # ec5de638 - delete
-# 8de7e630 ab15db82 b7c04d31 ca8d5c9d cb2f86c9
+# 8de7e630 b7c04d31 ca8d5c9d cb2f86c9 fba0fc0e
+# 51727c4c scratch
 
 python3 reward_transfer/experiments.py \
-  --trial_id 30ca9764 \
-  --substrate commons_harvest__open \
-  --n_iterations 500 \
-  --framework tf \
+  --trial_id fba0fc0e \
+  --substrate externality_mushrooms__dense \
+  --n_iterations 300 \
+  --framework tf2 \
   --num_cpus 16 \
   --num_gpus 1 \
-  --local_dir $SCRATCH/new \
+  --local_dir $SCRATCH/tf_2_14 \
   --rollout_workers 15 \
   --episodes_per_worker 2 \
   --max_concurrent_trials 1 \
-  --wandb new-harvest \
-  --training scratch \
-  --num_samples 2 \
-  --n 7 \
-  --s 1 \
+  --wandb mushroom-2-14 \
+  --training training \
+  --n 5 \
   --training_mode independent \
   --tmp_dir $SCRATCH/tmp # 1>/dev/null 2>&1
 
-
-
-#  --trial_id b7c04d31 \
-#  --substrate externality_mushrooms__dense \
-#  --n_iterations 300 \
-#  --framework tf2 \
-#  --local_dir $SCRATCH/tf_2_14 \
-#  --wandb mushroom-2-14 \
-#  --training pre-training \
 
 # CHECKPOINT_PATH=$SCRATCH/optuna/single/commons_harvest__open/PPO_ec9c9364_8000,9,0.00033,1,0.9,0.35,2_89_AlgorithmConfig__prior_exploration_config=None,disable_action_flattening=False,disable_2024-05-10_06-15-55/checkpoint_000000/policies/default
 # CHECKPOINT_PATH=$SCRATCH/optuna/random/commons_harvest__open/PPO_24778620_14000,15,0.0009,0.95,0.7,0.4,9_11_AlgorithmConfig__prior_exploration_config=None,disable_action_flattening=False,disa_2024-05-10_21-43-19/checkpoint_000000/policies/default
